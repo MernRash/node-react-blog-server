854bf5e11015d7dee3c56431646ea89ac581700a
MernRash <therahul.shrestha72744@gmail.com>
2022-01-14 23:23:01 +0530

    Resolved CORS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./express');
 
 
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT,()=>{
     console.log("Server Started here at Port Number 8000");
 })
