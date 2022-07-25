2505f3f8025feee232c00d0b7b9672fe42510eb4
Rahul Shrestha <88819251+MernRash@users.noreply.github.com>
2022-07-25 09:17:37 +0530

    Update express.js

--- express.js
+++ express.js
@@ -13,14 +13,7 @@ const app = express();
 app.use(express.json());
 
 const cors = require('cors');
-
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions))
+app.use(cors())
 
 const authRouter = require('./routes/auth/authLogin');
 
