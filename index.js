0b83ed621dcb9ccf75a963ce902549ec0c66d1dd
Rahul Shrestha <88819251+MernRash@users.noreply.github.com>
2022-07-27 08:47:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = require('./express');
 
 
 const PORT = process.env.PORT || 3001;
-app.listen(3001,()=>{
+app.listen(PORT,()=>{
     console.log("Server Started here at Port Number 3001");
 })
